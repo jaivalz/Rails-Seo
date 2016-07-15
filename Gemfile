8e378c386bb6e67052546e6a60522ef04a2b2257
Jaime <jaivalz@yahoo.com>
2016-07-15 20:29:40 +0000

    Added friendly_id in order to optimize seo-links

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'unicorn'
 
 gem 'ckeditor'
+gem 'friendly_id'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,20 @@
 class Post < ActiveRecord::Base
-  before_save :clean_links
-  
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+ before_save :clean_links
+
   validates_presence_of :title
   validates_presence_of :body
-  
+
   def clean_links
-    
+    # We use DocumentFragment so Nokogiri doesn't add DOCTYPE and other html.
     doc = Nokogiri::HTML::DocumentFragment.parse(body)
-    
+
     doc.css('a').each do |a|
       a[:rel] = 'nofollow'
       a[:target] = '_blank'
     end
-    
-    self.body = foc.to_s
-  end
-  
+
+    self.body = doc.to_s
+  end  
 end
