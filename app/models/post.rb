c124f0406b5f5326861ff51d86ccc9c2b49c3885
Jaime <jaivalz@yahoo.com>
2016-07-15 20:18:19 +0000

    Added ckeditor gem, added nokogiri for hyperlinks

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+  before_save :clean_links
+  
+  validates_presence_of :title
+  validates_presence_of :body
+  
+  def clean_links
+    
+    doc = Nokogiri::HTML::DocumentFragment.parse(body)
+    
+    doc.css('a').each do |a|
+      a[:rel] = 'nofollow'
+      a[:target] = '_blank'
+    end
+    
+    self.body = foc.to_s
+  end
+  
 end
